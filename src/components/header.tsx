5c2dadb275ba595da39425ba3bf931fd430bf07f
= <=>
2025-08-08 10:48:29 -0300

    feat: add most readed mangas section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "@tanstack/react-router";
 
 export default function HeaderComponent() {
     return (
-        <div className="w-full  text-gray-300 bg-gray-950  flex flex-col  items-center p-4">
+        <div className="w-full  text-gray-300 bg-gray-950/95  flex flex-col  items-center p-4 fixed top-0 right-0 z-30">
             <div className="max-w-[1200px] flex justify-between items-center w-full">
                 <div className="flex items-center space-x-8">
                     <Link to={"/"} className="font-bold outfit-bold ">NOX</Link>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,9 +5,9 @@ import HeaderComponent from '../components/header'
 export const Route = createRootRoute({
     component: () => (
         <>
-            <div className='w-screen h-screen overflow-hidden bg-gray-950 dark flex flex-col  items-center relative'>
+            <div className='w-screen h-screen overflow-x-hidden overflow-y-scroll bg-gray-950 dark flex flex-col  items-center relative'>
                 <HeaderComponent></HeaderComponent>
-                <div className='flex flex-col w-full max-w-[1200px] '>
+                <div className='flex flex-col w-full max-w-[1200px]  pt-20 '>
                     <Outlet />
                 </div>
             </div>
